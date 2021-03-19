a5e4ecc2ae16fd5b422e281b0ebe801e945b2611
juzinha <julianatelles.contato@gmail.com>
2021-03-19 15:50:41 -0300

    improving the layout

---
+++ src/components/title.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+
+export default class Title extends Component {
+
+  render() {
+    return (
+      <div className="title-div">
+        <h1>Calculadora de Salário Líquido Mensal</h1>
+        <h3>Escreva o valor de seu salário mensal abaixo</h3>
+      </div>
+    )
+  }
+}
