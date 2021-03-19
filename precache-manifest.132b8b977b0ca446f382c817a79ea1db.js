a73aeee3fe73230c52f7caa5cf47f3ade6299a60
juzinha <julianatelles.contato@gmail.com>
2021-03-19 14:48:26 -0300

    Updates

---
+++ precache-manifest.132b8b977b0ca446f382c817a79ea1db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4839c2e99401f63d4a1b9eba2c0e88ac",
+    "url": "/salary-calculator/index.html"
+  },
+  {
+    "revision": "a6b267cde6d7ea3f7b9c",
+    "url": "/salary-calculator/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "24b922728d8974c05c9c",
+    "url": "/salary-calculator/static/css/main.946153c2.chunk.css"
+  },
+  {
+    "revision": "a6b267cde6d7ea3f7b9c",
+    "url": "/salary-calculator/static/js/2.44384d72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/salary-calculator/static/js/2.44384d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b922728d8974c05c9c",
+    "url": "/salary-calculator/static/js/main.fb08c9c8.chunk.js"
+  },
+  {
+    "revision": "272ba5505fbb7ec11c09",
+    "url": "/salary-calculator/static/js/runtime-main.bee5d591.js"
+  }
+]);
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import SalaryCalculator from './components/salarycalculator';
-
-export default class App extends Component {
-  render() {
-    return <SalaryCalculator />;
-  }
-}
